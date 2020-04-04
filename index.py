482b62390a3f054158992c443af809c23f5b8093
John Doe <gautamgupta9298@gmail.com>
2020-04-04 16:30:31 -0400

    Added creds to json file

--- index.py
+++ index.py
@@ -4,6 +4,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+import json
+
+with open('credentials.json') as data_file:
+    data = json.load(data_file)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
@@ -19,9 +23,10 @@ sign_in_button.click()
 username_dialog = driver.find_element_by_name('login')
 password_dialog = driver.find_element_by_name('password')
 
-username_dialog.send_keys('andygautam4ever@gmail.com')
-password_dialog.send_keys('gautamzhang5ever')
+username_dialog.send_keys(data['username'])
+password_dialog.send_keys(data['password'])
 
-wait = WebDriverWait(driver, 5)
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.invisibility_of_element_located((By.ID, 'initial-loading')))
 login_button = wait.until(EC.element_to_be_clickable((By.ID, 'signin_btn')))
 login_button.click()
